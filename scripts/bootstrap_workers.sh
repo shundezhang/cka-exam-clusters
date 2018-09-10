95759ff299596f29b4af97dfba64244fc4a03775
Shunde Zhang <shunde.p.zhang@gmail.com>
2018-09-10 11:57:06 +1000

    Update bootstrap_workers.sh

--- scripts/bootstrap_workers.sh
+++ scripts/bootstrap_workers.sh
@@ -17,13 +17,13 @@
 # Download and install worker binaries
 
 wget -q --show-progress --https-only --timestamping \
+  https://github.com/containernetworking/plugins/releases/download/v0.7.1/cni-plugins-amd64-v0.7.1.tgz \
   https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kubectl \
   https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kube-proxy \
   https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kubelet
   #https://github.com/kubernetes-incubator/cri-tools/releases/download/v1.0.0-beta.0/crictl-v1.0.0-beta.0-linux-amd64.tar.gz \
   #https://storage.googleapis.com/kubernetes-the-hard-way/runsc \
   #https://github.com/opencontainers/runc/releases/download/v1.0.0-rc5/runc.amd64 \
-  #https://github.com/containernetworking/plugins/releases/download/v0.6.0/cni-plugins-amd64-v0.6.0.tgz \
   #https://github.com/containerd/containerd/releases/download/v1.1.0/containerd-1.1.0.linux-amd64.tar.gz \
 
 sudo mkdir -p \
@@ -40,7 +40,7 @@ sudo mkdir -p \
   #sudo mv kubectl kube-proxy kubelet runc runsc /usr/local/bin/
   sudo mv kubectl kube-proxy kubelet \/usr/local/bin/
   #sudo tar -xvf crictl-v1.0.0-beta.0-linux-amd64.tar.gz -C /usr/local/bin/
-  #sudo tar -xvf cni-plugins-amd64-v0.6.0.tgz -C /opt/cni/bin/
+  sudo tar -xvf cni-plugins-amd64-v0.7.1.tgz -C /opt/cni/bin/
   #sudo tar -xvf containerd-1.1.0.linux-amd64.tar.gz -C /
 }
 
