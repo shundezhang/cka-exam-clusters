54e60004ec449c38d57720391ec5628459e7c5c0
Shunde Zhang <shunde.p.zhang@gmail.com>
2018-09-10 11:44:40 +1000

    Update bootstrap_workers.sh

--- scripts/bootstrap_workers.sh
+++ scripts/bootstrap_workers.sh
@@ -3,6 +3,10 @@
 # Bootstrap the worker nodes
 # RUN ON ALL WORKERS!
 
+# add entries to /etc/hosts
+
+
+
 # Install OS dependencies
 
 {
@@ -13,14 +17,14 @@
 # Download and install worker binaries
 
 wget -q --show-progress --https-only --timestamping \
+  https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kubectl \
+  https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kube-proxy \
+  https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kubelet
   #https://github.com/kubernetes-incubator/cri-tools/releases/download/v1.0.0-beta.0/crictl-v1.0.0-beta.0-linux-amd64.tar.gz \
   #https://storage.googleapis.com/kubernetes-the-hard-way/runsc \
   #https://github.com/opencontainers/runc/releases/download/v1.0.0-rc5/runc.amd64 \
   #https://github.com/containernetworking/plugins/releases/download/v0.6.0/cni-plugins-amd64-v0.6.0.tgz \
   #https://github.com/containerd/containerd/releases/download/v1.1.0/containerd-1.1.0.linux-amd64.tar.gz \
-  https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kubectl \
-  https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kube-proxy \
-  https://storage.googleapis.com/kubernetes-release/release/v1.11.0/bin/linux/amd64/kubelet
 
 sudo mkdir -p \
   /etc/cni/net.d \
@@ -37,7 +41,7 @@ sudo mkdir -p \
   sudo mv kubectl kube-proxy kubelet \/usr/local/bin/
   #sudo tar -xvf crictl-v1.0.0-beta.0-linux-amd64.tar.gz -C /usr/local/bin/
   #sudo tar -xvf cni-plugins-amd64-v0.6.0.tgz -C /opt/cni/bin/
-  sudo tar -xvf containerd-1.1.0.linux-amd64.tar.gz -C /
+  #sudo tar -xvf containerd-1.1.0.linux-amd64.tar.gz -C /
 }
 
 # Configure CNI networking
@@ -46,23 +50,23 @@ POD_CIDR="10.200.0.0/24"
 
 # Create bridge
 
-cat <<EOF | sudo tee /etc/cni/net.d/10-bridge.conf
-{
-    "cniVersion": "0.3.1",
-    "name": "bridge",
-    "type": "bridge",
-    "bridge": "cnio0",
-    "isGateway": true,
-    "ipMasq": true,
-    "ipam": {
-        "type": "host-local",
-        "ranges": [
-          [{"subnet": "${POD_CIDR}"}]
-        ],
-        "routes": [{"dst": "0.0.0.0/0"}]
-    }
-}
-EOF
+#cat <<EOF | sudo tee /etc/cni/net.d/10-bridge.conf
+#{
+#    "cniVersion": "0.3.1",
+#    "name": "bridge",
+#    "type": "bridge",
+#    "bridge": "cnio0",
+#    "isGateway": true,
+#    "ipMasq": true,
+#    "ipam": {
+#        "type": "host-local",
+#        "ranges": [
+#          [{"subnet": "${POD_CIDR}"}]
+#        ],
+#        "routes": [{"dst": "0.0.0.0/0"}]
+#    }
+#}
+#EOF
 
 # Create loopback
 
@@ -73,45 +77,6 @@ cat <<EOF | sudo tee /etc/cni/net.d/99-loopback.conf
 }
 EOF
 
-# Configure containerd
-
-sudo mkdir -p /etc/containerd/
-
-cat << EOF | sudo tee /etc/containerd/config.toml
-[plugins]
-  [plugins.cri.containerd]
-    snapshotter = "overlayfs"
-    [plugins.cri.containerd.default_runtime]
-      runtime_type = "io.containerd.runtime.v1.linux"
-      runtime_engine = "/usr/local/bin/runc"
-      runtime_root = ""
-    [plugins.cri.containerd.untrusted_workload_runtime]
-      runtime_type = "io.containerd.runtime.v1.linux"
-      runtime_engine = "/usr/local/bin/runsc"
-      runtime_root = "/run/containerd/runsc"
-EOF
-
-cat <<EOF | sudo tee /etc/systemd/system/containerd.service
-[Unit]
-Description=containerd container runtime
-Documentation=https://containerd.io
-After=network.target
-
-[Service]
-ExecStartPre=/sbin/modprobe overlay
-ExecStart=/bin/containerd
-Restart=always
-RestartSec=5
-Delegate=yes
-KillMode=process
-OOMScoreAdjust=-999
-LimitNOFILE=1048576
-LimitNPROC=infinity
-LimitCORE=infinity
-
-[Install]
-WantedBy=multi-user.target
-EOF
 
 # Configure kubelet
 
